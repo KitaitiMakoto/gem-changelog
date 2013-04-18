23eeb6da2791cd9e2835befad5ca75b65cf54aa6
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-04-19 00:35:07 +0900

    Avoid showing changelong twice

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -12,12 +12,13 @@ module Gem::InstallUpdateOptions
     install_update_options_without_changelog
 
     add_option 'Install/Update', '--changelog[=LINES]', 'Show LINES lines the changelog of given gem, LINES defaults to 10' do |value, option|
+      next if @gem_changelog_lines
+      @gem_changelog_lines = (value && value =~ OptionParser::DecimalInteger) ? value.to_i : 10
       Gem.post_install do |installer|
-        lines = (value && value =~ OptionParser::DecimalInteger) ? value.to_i : 10
         command = Gem::Commands::ChangelogCommand.new
         changelog_file = options[:changelog_name] || command.find_changelog_file(installer.spec)
         next unless changelog_file
-        puts command.show_first_lines(File.join(installer.spec.gem_dir, changelog_file), lines)
+        puts command.show_first_lines(File.join(installer.spec.gem_dir, changelog_file), @gem_changelog_lines)
       end
     end
   end
